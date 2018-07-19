521a1d70a502891f816ec70c59b579b89fa31e25
GuilhermeRossin <37315196+GuilhermeRossin@users.noreply.github.com>
2018-07-18 21:36:08 -0300

    Create Display de 7 segmentos.ino

---
+++ Display de 7 segmentos.ino
@@ -0,0 +1,39 @@
+const int a = 12;
+const int b = 11;
+const int c = 10;
+const int d = 9;
+const int e = 8;
+const int f = 6;
+const int g = 7;
+
+void setup() {
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW);
+
+  delay(1000);
+
+  digitalWrite(a,LOW);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+
+  delay(1000);
+}
